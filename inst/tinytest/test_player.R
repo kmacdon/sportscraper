278667f7bff9ba4c5ea96a8aaa27a28784c0f862
Kevin MacDonald <kevinmacdonald4@gmail.com>
2020-03-02 23:11:32 -0600

    Fix file paths in test file

--- inst/tinytest/test_player.R
+++ inst/tinytest/test_player.R
@@ -1,31 +1,31 @@
-load("data/kb.RData")
-load("data/kb_adv.RData")
-load("data/jm.RData")
-load("data/bo.RData")
-load("data/dj.RData")
-load("data/ac.RData")
-load("data/ai_adv.RData")
+load("data/player/kb.RData")
+load("data/player/kb_adv.RData")
+load("data/player/jm.RData")
+load("data/player/bo.RData")
+load("data/player/dj.RData")
+load("data/player/ac.RData")
+load("data/player/ai_adv.RData")
 
-expect_equivalent(sportscraper:::nba_player("Kobe Bryant", xml2::read_html("data/kb.html"), advanced=FALSE), 
+expect_equivalent(sportscraper:::nba_player("Kobe Bryant", xml2::read_html("data/player/kb.html"), advanced=FALSE), 
                        kb)
 
-expect_equivalent(sportscraper:::nba_player("Kobe Bryant", xml2::read_html("data/kb_adv.html"), advanced=TRUE), 
+expect_equivalent(sportscraper:::nba_player("Kobe Bryant", xml2::read_html("data/player/kb_adv.html"), advanced=TRUE), 
                   kb_adv)
 
-expect_equivalent(sportscraper:::nfl_player("Joe Montana", xml2::read_html("data/jm.html")), 
+expect_equivalent(sportscraper:::nfl_player("Joe Montana", xml2::read_html("data/player/jm.html")), 
                   jm)
 
-expect_equivalent(sportscraper:::nhl_player("Bobby Or", xml2::read_html("data/bo.html")), 
+expect_equivalent(sportscraper:::nhl_player("Bobby Or", xml2::read_html("data/player/bo.html")), 
                   bo)
 
-expect_equivalent(sportscraper:::mlb_player("Derek Jeter", xml2::read_html("data/dj.html"), advanced=FALSE), 
+expect_equivalent(sportscraper:::mlb_player("Derek Jeter", xml2::read_html("data/player/dj.html"), advanced=FALSE), 
                   dj)
 
-expect_equivalent(sportscraper:::mlb_player("Derek Jeter", xml2::read_html("data/dj_adv.html"), advanced=TRUE), 
+expect_equivalent(sportscraper:::mlb_player("Derek Jeter", xml2::read_html("data/player/dj_adv.html"), advanced=TRUE), 
                   dj_adv)
 
-expect_equivalent(sportscraper:::cbb_player("Austin Carr", xml2::read_html("data/ac.html"), advanced=FALSE), 
+expect_equivalent(sportscraper:::cbb_player("Austin Carr", xml2::read_html("data/player/ac.html"), advanced=FALSE), 
                   ac)
 
-expect_equivalent(sportscraper:::cbb_player("Allen Iverson", xml2::read_html("data/ai_adv.html"), advanced=TRUE), 
+expect_equivalent(sportscraper:::cbb_player("Allen Iverson", xml2::read_html("data/player/ai_adv.html"), advanced=TRUE), 
                   ai_adv)
